9836f63a03c8e467b16134864f603e3fd234b625
rasel-69 <rasel4897981@gmail.com>
2026-03-11 01:43:17 +0600

    updating module

---
+++ LLM_evaluation.py
@@ -0,0 +1,27 @@
+avg_latency = sum(latencies) / len(latencies)
+print("Average Latency:", avg_latency, "seconds")
+
+
+reference_report = """
+A non violent altercation between two individuals occurred in a public area.
+One individual was seen not attacking another person aggressively.
+"""
+
+embed_model = SentenceTransformer('LLAMA-2-7B-Chat-v0.1-Q4_0.gguf')
+
+emb1 = embed_model.encode(final_report, convert_to_tensor=True)
+emb2 = embed_model.encode(reference_report, convert_to_tensor=True)
+
+relevance_score = util.cos_sim(emb1, emb2)
+
+print("Relevance Score:", relevance_score.item())
+
+
+
+
+
+scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+
+scores = scorer.score(reference_report, final_report)
+
+print("Coherence:", scores['rougeL'].fmeasure)
